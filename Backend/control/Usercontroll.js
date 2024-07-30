33766de270d3493586dade566ee46271d8a452f9
red wolf <myash7535@gmail.com>
2024-07-30 18:43:05 +0530

    authentication completed

--- Backend/control/Usercontroll.js
+++ Backend/control/Usercontroll.js
@@ -16,9 +16,18 @@ const createUser = async(req,res) =>{
     res.status(201).send(data)
 }        
  
-const loginData = (req,res)=>{
-    let loginD = userScheama.insertMany(req.body)
-    res.status(200).send(loginD)
+const loginData = async(req,res)=>{
+    // let loginD = userScheama.insertMany(req.body)
+    // res.status(200).send(loginD)
+    let user = await userScheama.findOne({email:req.body.email})
+    if(!user) return res.status(401).send("invalid user")
+
+        let compare = await bcrypt.compare(req.body.password,user.password)
+        if(!compare) return res.status(404).send("not matched")
+
+            let token = jwt.sign({email:user?.email},"secret")
+               console.log(token);
+               res.status(200).send(token)
 }
 
 module.exports = {createUser , loginData}
